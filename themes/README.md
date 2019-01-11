62f1e9004565f2ecaed0577350d16eee1df583fc
Jonathan Head <jhead@dpw.lacounty.gov>
2019-01-10 17:45:03 -0800

    Including Wp-Content in Repo

---
+++ themes/README.md
@@ -0,0 +1,8 @@
+# sativaUnderstrap
+
+## Plugins installed
+
+- Advanced Custom Fields
+- Custom Post Type UI
+- Post in Page
+- WP-Optimize
---
+++ themes/understrap-child/inc/customizer.php
@@ -0,0 +1,27 @@
+<?php
+    function wpus_customize_register($wp_customize) {
+       // Home Page Section
+
+       $wp_customize->add_section('homepage', array (
+            'title'         => __( 'Home Page', 'understrap_child' ),
+            'description'   => sprintf(__('Options for Home Page', 'understrap_child')),
+            'priority'      => 130
+       ));
+
+       $wp_customize->add_setting('homepage_image', array(
+            'default' => get_stylesheet_directory_uri() . '/inc/img/homepage_header.jpg',
+            'type' => 'theme_mod'
+
+       ));
+
+       $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'homepage_image', array(
+            'label' => __( 'Homepage Image', 'understrap_child' ),
+            'section' => 'homepage',
+            'settings' => 'homepage_image',
+            'priority' => '1'
+        
+        )));
+
+
+    }
+    add_action('customize_register', 'wpus_customize_register');
