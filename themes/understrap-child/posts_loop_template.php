466993b90e7c627a7a9efa228c5caed36503f405
cakeee <jonathan@owners-mbp.lan>
2019-01-12 00:05:24 -0800

    - Edited Post in Page Loop Template

--- themes/understrap-child/posts_loop_template.php
+++ themes/understrap-child/posts_loop_template.php
@@ -11,20 +11,34 @@
 	directory so your changes won't be overwritten when the plugin is upgraded. -->
 
 <!-- Post Wrap Start-->
-<div class="post hentry ivycat-post">
+<div class="news-loop page">
 	
 	<!-- 	This outputs the post TITLE -->
-	<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	<h3 class="text-seal-blue h4 font-weight-bold">
+		<?php the_title(); 
+		
+		if (get_field('update_subtitle')) {  ?>
+		<br><span>
+			<?php the_field('update_subtitle'); 
+		}?>
+		</span>
+	</h3>
+	<!-- <h2 class="entry-title"><a href="<?php //the_permalink(); ?>"><?php //the_title(); ?></a></h2> -->
 
 	<!-- 	This outputs the post EXCERPT.  To display full content including images and html, 
 		replace the_excerpt(); with the_content();  below. -->
 	<div class="entry-summary">
-		<?php the_excerpt(); ?>
+		<?php the_content(); ?>
 	</div>
 
 	<!--	This outputs the post META information -->
 	<div class="entry-utility">
-		<?php if ( count( get_the_category() ) ) : ?>
+		
+		<?php edit_post_link( __( 'Edit', 'posts-in-page' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
+	</div>
+</div>
+<!-- // Post Wrap End -->
+<!-- <?php /* if ( count( get_the_category() ) ) : ?>
 			<span class="cat-links">
 				<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'posts-in-page' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
 			</span>
@@ -38,8 +52,4 @@
 			</span>
 			<span class="meta-sep">|</span>
 		<?php endif; ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'posts-in-page' ), __( '1 Comment', 'posts-in-page' ), __( '% Comments', 'posts-in-page' ) ); ?></span>
-		<?php edit_post_link( __( 'Edit', 'posts-in-page' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
-	</div>
-</div>
-<!-- // Post Wrap End -->
+		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'posts-in-page' ), __( '1 Comment', 'posts-in-page' ), __( '% Comments', 'posts-in-page' ) ); */?></span> -->
